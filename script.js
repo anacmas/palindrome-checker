c57b4dc8c8abde4098ca0012a93bf56d0000c099
anasilva <anacmas@gmail.com>
2023-01-17 12:42:18 -0300

    feat: import variables to js and create click func

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".button-check"),
+  answer = document.querySelector(".answer"),
+  word = document.querySelector(".input-word").value;
+
+button.addEventListener("click", function () {
+  // word.toUpperCase();
+  console.log(word);
+});
